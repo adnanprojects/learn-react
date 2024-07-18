4ea45d13e04e48db6e54745d145da830ba77e9a8
adnanprojects <adnanchaudhary99@gmail.com>
2024-07-18 23:37:14 +0530

    Upate Shimmer

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -13,6 +13,8 @@ const Shimmer = () => {
             <div className="shimmer-card"></div>
             <div className="shimmer-card"></div>
             <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
         </div>
     );
 }
